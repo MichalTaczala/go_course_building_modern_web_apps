2ee776d58a01b9dcc6f4c89b1f6e87b9a3b83a77
MichalTaczala <michtaczala@gmail.com>
2023-09-03 18:17:54 +0200

    rendering templates format v1

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -6,11 +6,47 @@ import (
 	"net/http"
 )
 
-func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("error")
 		return
 	}
+}
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string) {
+	var tmpl *template.Template
+	var err error
+
+	_, inMap := tc[t]
+	if !inMap {
+		err = createTemplateCache(t)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+	}
+	tmpl = tc[t]
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+}
+
+func createTemplateCache(t string) error {
+	templates := []string{
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.tmpl",
+	}
+	tmpl, err := template.ParseFiles(templates...)
+	if err != nil {
+		return err
+	}
+	tc[t] = tmpl
+	return nil
+
 }
