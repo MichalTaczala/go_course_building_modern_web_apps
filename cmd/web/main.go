801cf13fc39890da6dabbff5745300a393e3d7a4
MichalTaczala <michtaczala@gmail.com>
2023-09-05 22:40:57 +0200

    add sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,16 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/MichalTaczala/go_course_building_modern_web_apps/pkg/config"
 	"github.com/MichalTaczala/go_course_building_modern_web_apps/pkg/handlers"
 	"github.com/MichalTaczala/go_course_building_modern_web_apps/pkg/render"
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8070"
 
+var app config.AppConfig
+
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction //true in production
+
+	app.Session = session
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal(err)
